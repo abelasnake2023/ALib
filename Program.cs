c6573d0d29d6361dc99ba0a8b4c9172a7040e1ee
Abel Asnake <abelasnake2003@gmail.com>
2024-01-21 13:07:27 +0300

    4th commit, just practicing git

--- Program.cs
+++ Program.cs
@@ -8,11 +8,6 @@ using System;
 
 public class Program
 {
-    public Program()
-    {
-        Console.WriteLine("Program()");
-    }
-
     public static void Main()
     {
         Console.WriteLine("Hello ALib!");
