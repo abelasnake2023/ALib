e437a49de38109c30e7dc18fd1f06f73cc499cec
Abel Asnake <abelasnake2003@gmail.com>
2024-01-21 13:04:29 +0300

    second commit, class Program organized

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿namespace ALib;
+
+
+
+using System;
+
+
+
+public class Program
+{
+    public Program()
+    {
+        
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Hello ALib!");
+    }
+}
